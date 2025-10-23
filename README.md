88c583fc6f0b905bb56f1b9d4dc0a780680611c0
azubirif <alejandrozubirifunes@gmail.com>
2025-10-23 17:19:39 +0200

    Interactable

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # project-fraus
+
+# Codebase
+## Pickup Objects
+El jugador está constantemente lanzando raycasts. Si impacta con un objeto que implemente "pickable", esto se mostrará con un shader,
+y si el jugador presiona la tecla de interacción, podrá cogerlo. Esto implica que la posición del objeto lerpeará a una posición
+específica delante del jugador. Estos objetos serán del tipo "Rigidbody". Al tenerlos sostenidos, su gravedad se apagará.
--- src/scripts/player_controller.gd
+++ src/scripts/player_controller.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
 @onready var head = $head
+@onready var cam = $head/Camera3D
 
 enum PlayerState {CanMove, Interacting}
 
@@ -8,6 +9,7 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 @export var mouse_sens = 0.2
+@export var ray_length = 5.0
 
 var player_state = PlayerState.CanMove
 
@@ -19,9 +21,15 @@ func _input(event: InputEvent) -> void:
 		rotate_y(-deg_to_rad(event.relative.x * mouse_sens))
 		head.rotate_x(-deg_to_rad(event.relative.y * mouse_sens))
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-50), deg_to_rad(0))
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+
+func _process(delta: float) -> void:
+	handle_zoom(delta)
 
 func _physics_process(delta: float) -> void:
 	_handle_player_movement(delta)
+	_handle_raycasts(delta)
 
 func _handle_player_movement(delta: float) -> void:
 	if player_state != PlayerState.CanMove:
@@ -47,3 +55,31 @@ func _handle_player_movement(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func handle_zoom(delta: float):
+	var base_zoom = 60.0
+	var zoom = 40.0
+	var zoom_speed = 4.0
+
+	if Input.is_action_pressed("zoom"):
+		cam.fov = lerp(cam.fov, zoom, delta * zoom_speed)
+	else:
+		cam.fov = lerp(cam.fov, base_zoom, delta * zoom_speed)
+
+func _handle_raycasts(delta: float):
+	var space_state = get_world_3d().direct_space_state
+	var mousepos = get_viewport().get_mouse_position()
+
+	var origin = cam.project_ray_origin(mousepos)
+	var end = origin + cam.project_ray_normal(mousepos) * ray_length
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	query.collide_with_areas = true
+
+	var result = space_state.intersect_ray(query)
+	if result != {}:
+		if result.collider is Object:
+			var object = result.collider
+			if object.has_method("interact"):
+				print("Interactable")
+			else:
+				print("Cant interact")
